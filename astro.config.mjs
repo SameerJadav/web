e094abe8406bacd6771fd48796732b1e0b85f779
Sameer Jadav <sameerjadav010@gmail.com>
2024-09-25 10:32:08 +0530

    feat: generate static site

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,7 +1,6 @@
 import { defineConfig } from "astro/config";
 import tailwind from "@astrojs/tailwind";
 import rehypePrettyCode from "rehype-pretty-code";
-import vercel from "@astrojs/vercel/serverless";
 
 import sitemap from "@astrojs/sitemap";
 import { SITE } from "./src/config";
@@ -9,12 +8,7 @@ import { SITE } from "./src/config";
 // https://astro.build/config
 export default defineConfig({
   integrations: [tailwind(), sitemap()],
-  output: "hybrid",
-  adapter: vercel({
-    webAnalytics: {
-      enabled: true,
-    },
-  }),
+  output: "static",
   site: SITE.url,
   markdown: {
     syntaxHighlight: false,
